139957cb1d8b3d55d47016984adfb86098f02d09
EmeraldNZ <ahyuancy@gmail.com>
2019-01-03 10:50:39 +1300

    Update .js

--- .js
+++ .js
@@ -1,3 +1,5 @@
+/************  Solution #1    ***************************************************************************************************/
+
 function binaryAgent(str) {
  //Use String.fromCharCode() to convert each ASCII number into the corresponding character & .map() to process each element from binary to decimal using pareseInt()      
    return String.fromCharCode(...str.split(' ').map(subStr => parseInt(subStr, 2)));
@@ -8,7 +10,21 @@ binaryAgent("01000001 01110010 01100101 01101110 00100111 01110100 00100000 0110
 binaryAgent("01001001 00100000 01101100 01101111 01110110 01100101 00100000 01000110 01110010 01100101 01100101 01000011 01101111 01100100 01100101 01000011 01100001 01101101 01110000 00100001"); //return "I love FreeCodeCamp!"
 
 
+/************  Solution #2    ***************************************************************************************************/
 
+function binaryAgent2(str) {
+    let arr = str.split(' ');  
+    let temp = 0;
+    let tempArr = [];
+    for(let i = 0; i < arr.length; i++){
+        for(let j = 7; j >= 0; j--){
+             temp += arr[i][j]  * Math.pow(2, (7-j));
+        }
+        tempArr.push(String.fromCharCode(temp));
+        temp = 0;
+    }
+    return tempArr.join('');
+}
 
 
 
